6d6123ee2033684211d83a18d68a7cec538c28c6
Kailey Lampert <trepmal@gmail.com>
2024-07-30 15:23:12 -0700

    init

---
+++ script.php
@@ -0,0 +1,300 @@
+<?php
+
+$return = [
+	'items' => []
+];
+
+$return['items'][] = [
+	'title' => 'fab-llama',
+	'arg'   => mosaic( 'fab-llama', 4, 5 ),
+	'icon'  => [
+		'path' => 'icons/fab-llama.png',
+	],
+];
+
+$return['items'][] = [
+	'title' => 'old-man',
+	'arg'   => ':oldman::old-man-yells-at-1:'.PHP_EOL.':old-man-yells-at-2::old-man-yells-at-3:',
+	'icon'  => [
+		'path' => 'icons/old-man.png',
+	],
+];
+
+$return['items'][] = [
+	'title' => 'bigger-oof',
+	'arg'   => ':bigger-oof1::bigger-oof2::bigger-oof3:'.PHP_EOL.':bigger-oof4::bigger-oof5::bigger-oof6:'.PHP_EOL.':bigger-oof7::bigger-oof8::bigger-oof9:',
+	'icon'  => [
+		'path' => 'icons/bigger-oof.png',
+	],
+];
+
+$return['items'][] = [
+	'title' => 'biggest-oof',
+	'arg'   => ':biggest-oof1::biggest-oof2::biggest-oof3::biggest-oof4::biggest-oof5:'.PHP_EOL.':biggest-oof6::biggest-oof7::biggest-oof8::biggest-oof9::biggest-oof10:'.PHP_EOL.':biggest-oof11::biggest-oof12::biggest-oof13::biggest-oof14::biggest-oof15:'.PHP_EOL.':biggest-oof16::biggest-oof17::biggest-oof18::biggest-oof19::biggest-oof20:',
+	'icon'  => [
+		'path' => 'icons/biggest-oof.png',
+	],
+];
+
+$return['items'][] = [
+	'title' => 'biggest-eyeroll',
+	'arg'   => mosaic('biggest-eyeroll', 4, 4),
+	'icon'  => [
+		'path' => 'icons/biggest-eyeroll.png',
+	],
+];
+$return['items'][] = [
+	'title' => 'bighug',
+	'arg'   => mosaic('bighug', 4, 4),
+	'icon'  => [
+		'path' => 'icons/bighug.png',
+	],
+];
+$return['items'][] = [
+	'title' => 'bigshrug',
+	'arg'   => mosaic('bigshrug', 4, 4),
+	'icon'  => [
+		'path' => 'icons/bigshrug.png',
+	],
+];
+
+$return['items'][] = [
+	'title' => 'aaaaaa',
+	'arg'   => mosaic('aaaaaa', 4, 4),
+	'icon'  => [
+		'path' => 'icons/aaaaaa.png',
+	],
+];
+
+$return['items'][] = [
+	'title' => 'dogrun',
+	'arg'   => mosaic('dogrun', 0, 6),
+	'icon'  => [
+		'path' => 'icons/dogrun.png',
+	],
+];
+$return['items'][] = [
+	'title' => 'longcat',
+	'arg'   => mosaic('longcat', 3, 1),
+	'icon'  => [
+		'path' => 'icons/longcat.gif',
+	],
+];
+$return['items'][] = [
+	'title' => 'bigsigh',
+	'arg'   => mosaic('bigsigh', 4, 5),
+	'icon'  => [
+		'path' => 'icons/bigsigh.png',
+	],
+];
+$return['items'][] = [
+	'title' => 'bigbutter',
+	'arg'   => mosaic('bigbutter', 3, 4),
+	'icon'  => [
+		'path' => 'icons/bigbutter.png',
+	],
+];
+$return['items'][] = [
+	'title' => 'bigpanic',
+	'arg'   => mosaic('bigpanic', 3, 5),
+	'icon'  => [
+		'path' => 'icons/bigpanic.png',
+	],
+];
+$return['items'][] = [
+	'title' => 'biggest-dumpsterfire',
+	'arg'   => mosaic('biggest-dumpsterfire', 4, 4),
+	'icon'  => [
+		'path' => 'icons/dumpsterfire.png',
+	],
+];
+$return['items'][] = [
+	'title' => 'biggest-dumpsterfire-real',
+	'arg'   => mosaic('biggest-dumpsterfire-real', 2, 2),
+	'icon'  => [
+		'path' => 'icons/dumpsterfire.png',
+	],
+];
+$return['items'][] = [
+	'title' => 'biggerest-shitstorm',
+	'arg'   => mosaic('biggerest-shitstorm', 14, 10),
+	'icon'  => [
+		'path' => 'icons/biggerest-shitstorm.png',
+	],
+];
+
+$return['items'][] = [
+	'title' => 'biggest-this-is-fine',
+	'arg'   => mosaic('biggest-this-is-fine', 4, 7),
+	'icon'  => [
+		'path' => 'icons/this-is-fine.jpg',
+	],
+];
+
+$return['items'][] = [
+	'title' => 'train',
+	'arg'   => ':train-engine::train-boxcar::train-flatcar::train-flatcar::train-flatcar::train-boxcar::train-caboose:',
+	'icon'  => [
+		'path' => 'icons/train.png',
+	],
+];
+
+$return['items'][] = [
+	'title' => 'longdog',
+	'arg'   => ':longdog-head::longdog-body::longdog-body::longdog-body::longdog-rear:',
+	'icon'  => [
+		'path' => 'icons/longdog.png',
+	],
+];
+
+$return['items'][] = [
+	'title' => 'boop',
+	'arg'   => ':boop-1-1::boop-2-1::boop-3-1:'.PHP_EOL.':boop-1-15::boop-2-15::boop-3-15:'.PHP_EOL.':boop-1-15::boop-2-15::boop-3-15:'.PHP_EOL.':boop-1-15::boop-2-15::boop-3-15:'.PHP_EOL.':boop-1-15::boop-2-15::boop-3-15:'.PHP_EOL.':boop-1-15::boop-2-15::boop-3-15:'.PHP_EOL.':boop-1-15::boop-2-15::boop-3-15:'.PHP_EOL.':boop-1-15::boop-2-15::boop-3-15:'.PHP_EOL.':boop-1-15::boop-2-15::boop-3-15:'.PHP_EOL.':boop-1-15::boop-2-15::boop-3-15:'.PHP_EOL.':boop-1-15::boop-2-15::boop-3-15:'.PHP_EOL.':boop-1-2::boop-2-2::boop-3-2:',
+	'icon'  => [
+		'path' => 'icons/boop.png',
+	],
+];
+
+$return['items'][] = [
+	'title' => 'squid',
+	'arg'   => ':squid-left::squid-arm::squid-arm::squid-body::squid-arm::squid-arm::squid-arm::squid-arm::squid-arm::squid-right:',
+	'icon'  => [
+		'path' => 'icons/squid.png',
+	],
+];
+
+$return['items'][] = [
+	'title' => 'big-if',
+	'arg'   => mosaic( 'big-if', 4, 6 ),
+	'icon'  => [
+		'path' => 'icons/big-if.png',
+	],
+];
+$return['items'][] = [
+	'title' => 'timezones',
+	'arg'   => mosaic( 'biggest-timezones', 5, 5 ),
+	'icon'  => [
+		'path' => 'icons/timezones.png',
+	],
+];
+
+$return['items'][] = [
+	'title' => 'bigger-if',
+	'arg'   => mosaic( 'bigger-if', 8, 12 ),
+	'icon'  => [
+		'path' => 'icons/big-if.png',
+	],
+];
+
+$return['items'][] = [
+	'title' => 'honk-bomb',
+	'arg'   => ':honk-bomb-0-0::honk-bomb-0-1::honk-bomb-0-2::honk-bomb-0-3::honk-bomb-0-4::honk-bomb-0-5::honk-bomb-0-6::honk-bomb-0-7:
+	:honk-bomb-1-0::honk-bomb-1-1::honk-bomb-1-2::honk-bomb-1-3::honk-bomb-1-4::honk-bomb-1-5::honk-bomb-1-6::honk-bomb-1-7:
+	:honk-bomb-2-0::honk-bomb-2-1::honk-bomb-2-2::honk-bomb-2-3::honk-bomb-2-4::honk-bomb-2-5::honk-bomb-2-6::honk-bomb-2-7:
+	:honk-bomb-3-0::honk-bomb-3-1::honk-bomb-3-2::honk-bomb-3-3::honk-bomb-3-4::honk-bomb-3-5::honk-bomb-3-6::honk-bomb-3-7:',
+	'icon'  => [
+		'path' => 'icons/honk.png',
+	],
+];
+
+
+/**
+            just bee movie below.....
+                                           **/
+
+
+$return['items'][] = [
+	'title' => 'beemovie-part-1',
+	'arg'   => mosaic('beemovie-part-1', 2, 4),
+	'icon'  => [
+		'path' => 'icons/beemovie.png',
+	],
+];
+$return['items'][] = [
+	'title' => 'beemovie-part-2',
+	'arg'   => mosaic('beemovie-part-2', 2, 4),
+	'icon'  => [
+		'path' => 'icons/beemovie.png',
+	],
+];
+$return['items'][] = [
+	'title' => 'beemovie-part-3',
+	'arg'   => mosaic('beemovie-part-3', 2, 4),
+	'icon'  => [
+		'path' => 'icons/beemovie.png',
+	],
+];
+$return['items'][] = [
+	'title' => 'beemovie-part-4',
+	'arg'   => mosaic('beemovie-part-4', 2, 4),
+	'icon'  => [
+		'path' => 'icons/beemovie.png',
+	],
+];
+$return['items'][] = [
+	'title' => 'beemovie-part-5',
+	'arg'   => mosaic('beemovie-part-5', 2, 4),
+	'icon'  => [
+		'path' => 'icons/beemovie.png',
+	],
+];
+$return['items'][] = [
+	'title' => 'beemovie-part-6',
+	'arg'   => mosaic('beemovie-part-6', 2, 4),
+	'icon'  => [
+		'path' => 'icons/beemovie.png',
+	],
+];
+$return['items'][] = [
+	'title' => 'beemovie-part-7',
+	'arg'   => mosaic('beemovie-part-7', 2, 4),
+	'icon'  => [
+		'path' => 'icons/beemovie.png',
+	],
+];
+$return['items'][] = [
+	'title' => 'beemovie-part-8',
+	'arg'   => mosaic('beemovie-part-8', 2, 4),
+	'icon'  => [
+		'path' => 'icons/beemovie.png',
+	],
+];
+
+
+/**
+ * 
+ * @param string $name Base name of image
+ * @param int $rows Number of rows. Use 0 to indicate that there's only 1 row, but it should not be indicated in the name e.g. mosaic( 'dogrun', 0, 4 );
+ * @param int $cols Number of cols. Use 0 to indicate that there's only 1 col, but it should not be indicated in the name
+ * @return string
+ */
+function mosaic( $name, $rows=3, $cols=4 ) {
+
+	$print_row = $print_col = true;
+
+	if ( $cols == 0 ) {
+		$print_col = false;
+		$cols = 1;
+	}
+	if ( $rows == 0 ) {
+		$print_row = false;
+		$rows = 1;
+	}
+
+	$output = '';
+
+	for ( $x=1; $x<=$rows; $x++ ) {
+		for ( $y=1; $y<=$cols; $y++ ) {
+			$output .= sprintf(
+				":%s%s%s:",
+				$name,
+				( $print_row ? "-$x" : "" ),
+				( $print_col ? "-$y" : "" )
+			);
+		}
+		$output .= PHP_EOL;
+	}
+
+	return $output;
+}
+die( json_encode( $return ) );
